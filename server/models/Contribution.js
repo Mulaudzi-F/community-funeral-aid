3ee4410d5b28210d17f9610026aa9140a50a13d3
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-04-15 23:18:00 +0200

    Added the model for calculating the contributions

---
+++ server/models/Contribution.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const ContributionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    community: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+      required: true,
+    },
+    section: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Section",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    transactionId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "completed", "failed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Contribution", ContributionSchema);
